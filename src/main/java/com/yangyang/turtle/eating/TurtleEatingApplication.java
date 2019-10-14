94297905f7ce33eedffeeae36c26511bf2d9fa77
kekecany <kekecany@163.com>
2019-10-14 22:34:12 +0800

    project init

---
+++ src/main/java/com/yangyang/turtle/eating/TurtleEatingApplication.java
@@ -0,0 +1,13 @@
+package com.yangyang.turtle.eating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurtleEatingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurtleEatingApplication.class, args);
+	}
+
+}
